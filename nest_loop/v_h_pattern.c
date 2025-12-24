d6aa0b6dc497a15b52cfa7aa7ff91dda3d304125
Tushar1234shinde <100269635+Tushar1234shinde@users.noreply.github.com>
2025-12-24 17:47:03 +0530

    Add files via upload

---
+++ nest_loop/v_h_pattern.c
@@ -0,0 +1,13 @@
+//vertical and hotizontal pattern of *
+#include<stdio.h>
+void main(){
+    int i,j;
+    printf("\n Horizontal Star \n\n");
+    for(i=0;i<5;i++){
+        printf(" * ");
+    }
+        printf("\n\n Vertical Star \n");
+    for(i=0;i<5;i++){
+        printf("\n * ");
+    }
+}
