521e2a55820c5212ef91d4e0d5af18c13c34a49a
Tushar Shinde <100269635+Tushar1234shinde@users.noreply.github.com>
2026-02-06 12:10:59 +0530

    Add files via upload

--- C_DSA_Batch/passing_array_to_function/array3.c
+++ C_DSA_Batch/passing_array_to_function/array3.c
@@ -1,37 +1,37 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-void passArray(int a, int b, int array[a][b]);
+// void passArray(int a, int b, int array[a][b]);
 
-int main() {
-    int row, col;
+// int main() {
+//     int row, col;
 
-    printf("Enter value for row: ");
-    scanf("%d", &row);
+//     printf("Enter value for row: ");
+//     scanf("%d", &row);
 
-    printf("Enter value for column: ");
-    scanf("%d", &col);
+//     printf("Enter value for column: ");
+//     scanf("%d", &col);
 
-    int arr_2d[row][col];
+//     int arr_2d[row][col];
 
-    for(int i = 0; i < row; i++) {
-        for(int j = 0; j < col; j++) {
-            printf("Enter element for arr_2d[%d][%d]: ", i, j);
-            scanf("%d", &arr_2d[i][j]);
-        }
-    }
+//     for(int i = 0; i < row; i++) {
+//         for(int j = 0; j < col; j++) {
+//             printf("Enter element for arr_2d[%d][%d]: ", i, j);
+//             scanf("%d", &arr_2d[i][j]);
+//         }
+//     }
 
-    passArray(row, col, arr_2d);
-    return 0;
-}
+//     passArray(row, col, arr_2d);
+//     return 0;
+// }
 
-void passArray(int a, int b, int array[a][b]) {
-    printf("2D Array elements are:\n");
-    for(int i = 0; i < a; i++) {
-        for(int j = 0; j < b; j++) {
-            printf("Element [%d][%d]: %d\n", i, j, array[i][j]);
-        }
-    }
-}
+// void passArray(int a, int b, int array[a][b]) {
+//     printf("2D Array elements are:\n");
+//     for(int i = 0; i < a; i++) {
+//         for(int j = 0; j < b; j++) {
+//             printf("Element [%d][%d]: %d\n", i, j, array[i][j]);
+//         }
+//     }
+// }
 
 
 // #include<stdio.h>
